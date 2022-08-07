c95e3f5436e8b149fba9575dccb31fed26953402
Jonathan <jonpdw@gmail.com>
2022-08-07 23:36:02 +1200

    remove swagger

--- NetlogixOrderApi/Program.cs
+++ NetlogixOrderApi/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<OrderDb>(options => options.UseInMemoryDatabase("items"));
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
@@ -12,8 +11,6 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    app.UseSwaggerUI();
-    app.UseSwagger();
 }
 
 app.UseAuthorization();
